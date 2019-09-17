f9eb0d0950fdf37b8f41220d0a35485992902696
salam <salam73@gmail.com>
2019-09-17 06:50:30 -0400

    add some date

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -1,16 +1,11 @@
 import 'package:flutter/material.dart';
 
-
 import 'dog_model.dart';
 import 'dog_list.dart';
 import 'new_dog_form.dart';
 
-
-
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-
 
   final String title;
 
@@ -19,9 +14,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
-
   Future _showNewDogForm() async {
     // push a new route like you did in the last section
     Dog newDog = await Navigator.of(context).push(
@@ -40,8 +32,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   List<Dog> initialDoggos = []
     ..add(Dog('NiceOne', 'Portland, OR, USA',
+        'Ruby is a very good girl. Yes: Fetch, loungin\'. No: Dogs who get on furniture.'))
+    ..add(Dog('NiceOne', 'Portland, OR, USA',
+        'Ruby is a very good girl. Yes: Fetch, loungin\'. No: Dogs who get on furniture.'))
+    ..add(Dog('NiceOne', 'Portland, OR, USA',
         'Ruby is a very good girl. Yes: Fetch, loungin\'. No: Dogs who get on furniture.'));
-
 
   @override
   Widget build(BuildContext context) {
@@ -56,7 +51,6 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: _showNewDogForm,
           ),
         ],
-
       ),
       body: Container(
         // Add box decoration
