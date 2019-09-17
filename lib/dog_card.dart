53833395de297e6b897d391d61d0a351c84b0997
salam <salam73@gmail.com>
2019-09-17 00:33:35 -0400

    add dogCard and dogImage widget

--- lib/dog_card.dart
+++ lib/dog_card.dart
@@ -65,8 +65,75 @@ class _DogCardState extends State<DogCard> {
     );
   }
 
+  Widget get dogCard {
+    // A new container
+    // The height and width are arbitrary numbers for styling.
+    return Container(
+      width: 290.0,
+      height: 115.0,
+      child: Card(
+        color: Colors.black87,
+        // Wrap children in a Padding widget in order to give padding.
+        child: Padding(
+          // The class that controls padding is called 'EdgeInsets'
+          // The EdgeInsets.only constructor is used to set
+          // padding explicitly to each side of the child.
+          padding: const EdgeInsets.only(
+            top: 8.0,
+            bottom: 8.0,
+            left: 64.0,
+          ),
+          // Column is another layout widget -- like stack -- that
+          // takes a list of widgets as children, and lays the
+          // widgets out from top to bottom.
+          child: Column(
+            // These alignment properties function exactly like
+            // CSS flexbox properties.
+            // The main axis of a column is the vertical axis,
+            // `MainAxisAlignment.spaceAround` is equivalent of
+            // CSS's 'justify-content: space-around' in a vertically
+            // laid out flexbox.
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text(widget.dog.name,
+                  // Themes are set in the MaterialApp widget at the root of your app.
+                  // They have default values -- which we're using because we didn't set our own.
+                  // They're great for having consistent, app-wide styling that's easily changed.
+                  style: Theme.of(context).textTheme.headline),
+              Text(widget.dog.location,
+                  style: Theme.of(context).textTheme.subhead),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.star,
+                  ),
+                  Text(': ${widget.dog.rating} / 10')
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text(widget.dog.name);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        height: 115.0,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: 50.0,
+              child: dogCard,
+            ),
+            Positioned(top: 7.5, child: dogImage),
+          ],
+        ),
+      ),
+    );
   }
 }
