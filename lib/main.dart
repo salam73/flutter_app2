c005029d160fe7b7298a9c552adbe82d4c1c18a2
salam <salam73@gmail.com>
2019-09-16 23:30:57 -0400

    change text

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+             'hello world',
             ),
 
           ],
