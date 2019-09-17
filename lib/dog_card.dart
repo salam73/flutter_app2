be60473431c647d8e94c507e50be11617b95d0a2
salam <salam73@gmail.com>
2019-09-17 00:14:58 -0400

    There are two main parts to this card. The image, and the actual Card that sits under it.
    First, make that image.

--- lib/dog_card.dart
+++ lib/dog_card.dart
@@ -16,6 +16,42 @@ class _DogCardState extends State<DogCard> {
 
   _DogCardState(this.dog);
 
+
+
+
+
+  String renderUrl;
+
+  Widget get dogImage {
+    return Container(
+      // You can explicitly set heights and widths on Containers.
+      // Otherwise they take up as much space as their children.
+      width: 100.0,
+      height: 100.0,
+      // Decoration is a property that lets you style the container.
+      // It expects a BoxDecoration.
+      decoration: BoxDecoration(
+        // BoxDecorations have many possible properties.
+        // Using BoxShape with a background image is the
+        // easiest way to make a circle cropped avatar style image.
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          // Just like CSS's `imagesize` property.
+          fit: BoxFit.cover,
+          // A NetworkImage widget is a widget that
+          // takes a URL to an image.
+          // ImageProviders (such as NetworkImage) are ideal
+          // when your image needs to be loaded or can change.
+          // Use the null check to avoid an error.
+          image: NetworkImage(renderUrl ?? ''),
+        ),
+      ),
+    );
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Text(widget.dog.name);
