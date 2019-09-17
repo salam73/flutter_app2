5687c9bcb384a16a88d69082b2a18dca844ba750
salam <salam73@gmail.com>
2019-09-17 00:42:29 -0400

    more Data Gradients Background

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -21,6 +21,12 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   List<Dog> initialDoggos = []
+    ..add(Dog('NiceOne', 'Portland, OR, USA',
+        'Ruby is a very good girl. Yes: Fetch, loungin\'. No: Dogs who get on furniture.'))
+    ..add(Dog('Rex', 'Seattle, WA, USA', 'Best in Show 1999'))
+    ..add(Dog('Rod Stewart', 'Prague, CZ',
+        'Star good boy on international snooze team.'))
+    ..add(Dog('Herbert', 'Dallas, TX, USA', 'A Very Good Boy'))
     ..add(Dog('Ruby', 'Portland, OR, USA',
         'Ruby is a very good girl. Yes: Fetch, loungin\'. No: Dogs who get on furniture.'))
     ..add(Dog('Rex', 'Seattle, WA, USA', 'Best in Show 1999'))
@@ -28,7 +34,6 @@ class _MyHomePageState extends State<MyHomePage> {
         'Star good boy on international snooze team.'))
     ..add(Dog('Herbert', 'Dallas, TX, USA', 'A Very Good Boy'))
     ..add(Dog('Buddy', 'North Pole, Earth', 'Self proclaimed human lover.'));
-
 
   @override
   Widget build(BuildContext context) {
@@ -38,11 +43,26 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.black87,
       ),
       body: Container(
-        // Remove the DogCard Widget.
-        // Instead, use your new DogList Class,
-        // Pass in the mock data from the list above.
-        child: Center( // Changed code
-          child: DogList(initialDoggos), // Changed code
+        // Add box decoration
+        decoration: BoxDecoration(
+          // Box decoration takes a gradient
+          gradient: LinearGradient(
+            // Where the linear gradient begins and ends
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            // Add one stop for each color. Stops should increase from 0 to 1
+            stops: [0.1, 0.5, 0.7, 0.9],
+            colors: [
+              // Colors are easy thanks to Flutter's Colors class.
+              Colors.indigo[800],
+              Colors.indigo[700],
+              Colors.indigo[600],
+              Colors.indigo[400],
+            ],
+          ),
+        ),
+        child: Center(
+          child: DogList(initialDoggos),
         ),
       ),
     );
